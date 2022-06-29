0fd9d45d50e9f799c92664206d7e5e5fcdf0a660
andressa15alm <88633758+andressa15alm@users.noreply.github.com>
2022-06-29 09:45:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+//crie as variaveis
+
+
+function preload(){
+  //adicione a animação
+ 
+}
+
+function setup() {
+  createCanvas(600,200)
+  
+  //criar sprite do ground
+
+
+  //criar a sprite do trex
+
+}
+
+function draw() {
+  background(180);
+ //adicione o controle de pulo
+  
+  //adicione gravidade
+ 
+   
+  //adicione a colisão com o ground
+  
+    drawSprites();
+}
+
